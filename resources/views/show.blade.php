a4086e172b4f107b4df5ba334a733395dcb160fc
Emir Cica <emircica.ec@gmail.com>
2025-04-16 13:48:36 +0300

    Front-End yapılıyor, task page az kaldı

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,21 +2,44 @@
 @section('title', $task->title)
 
 @section('content')
-
-<p>{{ $task->description }}</p>
+    <div class="mb-4">
+        <a href="{{ route('tasks.index') }}" class="link">
+            🔙 Go Back to Task List
+        </a>
+    </div>
+<p class="mb-4 text-slate-700">{{ $task->description }}</p>
 
 @if(!empty($task->long_description))
-    <p>{{ $task->long_description }}</p>
+    <p class="mb-4 text-slate-700">{{ $task->long_description }}</p>
 @endif
 
-<p>Oluşturulma Tarihi: {{ $task->created_at->format('d.m.Y H:i') }}</p>
-<p>Güncellenme Tarihi: {{ $task->updated_at->diffForHumans() }}</p>
+<p class="mb-4 text-sm text-slate-500">Created {{ $task->created_at->diffForHumans()}} • Updated {{ $task->updated_at->diffForHumans() }}</p>
+<p class="mb-4">
+    @if($task->completed)
+        <span class="font-medium text-green-500">Completed</span>
+    @else
+       <span class="font-medium text-red-600">Incompleted</span>
+    @endif
+</p>
+<div class="flex gap-2">
+    <a href="{{ route('tasks.edit', ['task' => $task]) }}"
+       class="btn">
+       Edit
+    </a>
 
-<div>
-    <form action="{{route('tasks.destroy',['task' =>$task->id])}}" method="POST">
+    <form method="POST" action=" {{route('tasks.toggle-complete',['task'=>$task])}} ">
+        @csrf
+        @method('PUT')
+        <button type="submit"
+        class="btn">
+         Mark as {{ $task->completed ? ' Incompleted' : ' Completed' }}
+        </button>
+    </form>
+
+    <form action="{{route('tasks.destroy',['task' =>$task])}}" method="POST">
         @csrf
         @method('DELETE')
-        <button type="submit" >Delete</button>
+        <button type="submit" class="btn" >Delete</button>
     </form>
 </div>
 @endsection
